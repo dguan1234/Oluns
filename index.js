9dfe1e824df301fd39d2c23937a9389d64c8424e
forkyspoon <david.guan@sjsu.edu>
2022-06-22 12:44:52 -0700

    initial commit

---
+++ index.js
@@ -0,0 +1,32 @@
+$('img[data-toggle=popover]').popover({
+  html: true,
+  trigger: 'hover',
+  placement: 'bottom',
+  content: function(){return '<img src="assets/' + this.className +"-desc.JPG" + '" />';}
+});
+
+
+$('.hidden').fadeIn(1000).removeClass('hidden')
+
+
+
+$(window).on("scroll resize", function() {
+    $(".fade-in").each(function(index, element) {
+        if (isScrolledIntoView(element)) {
+           $(element).animate({opacity: 1.0}, 300);
+        }
+    });
+});
+
+
+
+function isScrolledIntoView(elem)
+{
+    var centerY = Math.max(0,(($(window).height()-$(elem).outerHeight()) / 2)
+                  + $(window).scrollTop());
+
+    var elementTop = $(elem).offset().top;
+    var elementBottom = elementTop + $(elem).height();
+
+    return elementTop <= centerY && elementBottom >= centerY;
+}
